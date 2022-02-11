cbb24912b89782463bfe3b649ea226cf7d94bc08
TSI-ashishsharma <sharma.a@t-systems.com>
2022-02-11 13:20:59 +0530

    add new package

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "test:unit":
+/*!*****************!*\
+  !*** test:unit ***!
+  \*****************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed: UnhandledSchemeError: Reading from \\\"test:unit\\\" is not handled by plugins (Unhandled scheme).\\nWebpack supports \\\"data:\\\" and \\\"file:\\\" URIs by default.\\nYou may need an additional plugin to handle \\\"test:\\\" URIs.\\n    at /var/www/server2/apps/photos/node_modules/webpack/lib/NormalModule.js:769:26\\n    at Hook.eval [as callAsync] (eval at create (/var/www/server2/apps/photos/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/var/www/server2/apps/photos/node_modules/tapable/lib/Hook.js:18:14)\\n    at Object.processResource (/var/www/server2/apps/photos/node_modules/webpack/lib/NormalModule.js:766:9)\\n    at processResource (/var/www/server2/apps/photos/node_modules/loader-runner/lib/LoaderRunner.js:220:11)\\n    at iteratePitchingLoaders (/var/www/server2/apps/photos/node_modules/loader-runner/lib/LoaderRunner.js:171:10)\\n    at runLoaders (/var/www/server2/apps/photos/node_modules/loader-runner/lib/LoaderRunner.js:397:2)\\n    at NormalModule.doBuild (/var/www/server2/apps/photos/node_modules/webpack/lib/NormalModule.js:756:3)\\n    at NormalModule.build (/var/www/server2/apps/photos/node_modules/webpack/lib/NormalModule.js:910:15)\\n    at /var/www/server2/apps/photos/node_modules/webpack/lib/Compilation.js:1312:12\");\n\n//# sourceURL=webpack://photos/test:unit?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["test:unit"]();
+/******/ 	
+/******/ })()
+;
